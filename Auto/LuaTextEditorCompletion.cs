b15bdbd97790fe0aa3e1dd1a83b48857f2dcaf1c
kobra <c0bra@xiatek.org>
2013-10-14 18:57:42 +0100

    Starting to impliment auto completion

--- Auto/LuaTextEditorCompletion.cs
+++ Auto/LuaTextEditorCompletion.cs
@@ -3,23 +3,43 @@ using System;
 using System.Text.RegularExpressions;
 using MonoDevelop.Ide.Gui.Content;
 using MonoDevelop.Ide.CodeCompletion;
+using MonoDevelop.Core;
 
 namespace LuaBinding
 {
 	public class LuaTextEditorCompletion : CompletionTextEditorExtension
 	{
+		Regex rx_is_local = new Regex( @"^\s*local\s+((([A-z_][A-z0-9_]*))(\s*,\s*([A-z_][A-z0-9_]*))*)?\s*$", RegexOptions.Compiled );
+		Regex rx_locals   = new Regex( @"local\s+(([A-z_][A-z0-9_]*))(\s*,\s*([A-z_][A-z0-9_]*))*", RegexOptions.Compiled );
 		public override bool CanRunCompletionCommand()
 		{
+			string line = this.Editor.GetLineText( this.Editor.Caret.Line );
+
+			{ // Are we in a definition?
+				string to_left = line.Substring( 0, Math.Min( this.Editor.Caret.Column, line.Length ) );
+				if( rx_is_local.IsMatch( to_left ) )
+					return false; // nope, we're defining a local variable
+			}
+
 			return true;
+		}
+
+		public override ICompletionDataList HandleCodeCompletion(CodeCompletionContext completionContext, char completionChar, ref int triggerWordLength)
+		{
+
+			return base.HandleCodeCompletion(completionContext, completionChar, ref triggerWordLength);
 		}
 
 		public override ICompletionDataList CodeCompletionCommand(CodeCompletionContext completionContext)
 		{
+
 			CompletionDataList ret = new CompletionDataList();
+
 			ret.Add( "table.insert" );
 			ret.Add( "table.remove" );
 			ret.Add( "print" );
 			ret.Add( "type" );
+
 
 			return ret;
 			//return base.CodeCompletionCommand(completionContext);
